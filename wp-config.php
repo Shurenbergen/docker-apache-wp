49460f5bb7ded0e77b5bff7cb0e5118e4479a8b9
Shurenbergen <Sham-aleksandr@yandex.ru>
2022-01-14 15:26:17 +0000

    Fix wp-config.php (DB_HOST), fix docker-compose (mysql container)

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wordpressuser' );
 define( 'DB_PASSWORD', '7k0xAS*x' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
